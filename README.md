78dd648c0efce8c46cadafcd2d60969878f6662b
Jake Bottrall <jakebottrall@gmail.com>
2024-02-18 13:41:55 -0800

    ubuntu setup script (#1)
    * wip
    
    * refinements
    
    * rename profiles to config
    
    * more refinements
    
    * remove default settings from .bashrc
    
    * use oh-my-bash
    
    * install tilix
    
    * organize apt installs
    
    * add tilix config
    
    * uncomment 1password install
    
    * add space
    
    * install steam
    
    * fix bash config cp
    
    * install and setup solaar
    
    * install gnome-tweaks and papirus icon theme
    
    * tidy up ubuntu setup script
    
    * add instructions for ubuntu
    
    * add space

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ Scripts to get my systems setup.
 ```sh
 git clone https://github.com/jakebottrall/setup-scripts.git
 cd setup-scripts
-chmod +x *.sh
 ./setup_mac.sh
+```
+
+## Ubuntu
+
+```sh
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
+cd setup-scripts
+./setup_ubuntu.sh
 ```
---
+++ setup_ubuntu.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+./setup_git.sh
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+curl https://pyenv.run | bash
+
+curl -fsSL https://bun.sh/install | bash
+
+sudo add-apt-repository ppa:solaar-unifying/stable
+
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+sudo apt install fonts-firacode -y
+sudo apt install gh -y
+sudo apt install gnome-tweaks -y
+sudo apt install golang -y
+sudo apt install htop -y
+sudo apt install papirus-icon-theme -y
+sudo apt install pipx -y
+sudo apt install postgresql -y
+sudo apt install rbenv -y
+sudo apt install solaar -y
+sudo apt install tilix -y
+sudo apt install transmission -y
+sudo apt install 1password -y
+
+sudo snap install code --classic
+sudo snap install discord
+sudo snap install docker
+sudo snap install spotify
+sudo snap install steam
+
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+
+# setup tilix
+dconf load /com/gexperts/Tilix/ < ./config/tilix.dconf
+
+# setup bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+cp -rf ./config/.bashrc ~/.bashrc
+source ~/.bashrc
+
+# setup node
+nvm install --lts
+nvm use --lts
+
+# setup ruby
+rbenv install 3.1.2
+rbenv global 3.1.2
+
+# setup python
+pyenv install 3.12
+pyenv global 3.12
+
+# setup poetry
+pipx install poetry
+
+# setup vs code
+./setup_code.sh
+
+# setup mx master 3
+cp -rf ./config/solaar_rules.yaml ~/.config/solaar/rules.yaml
