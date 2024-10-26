4bd5839bc5b83312c94ae7a83392c4e2a1d7785b
Jake Bottrall <jakebottrall@gmail.com>
2024-10-26 16:05:12 -0700

    revamp ubuntu setup

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -5,51 +5,49 @@ sudo apt upgrade -y
 
 ./setup_git.sh
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 
-sudo add-apt-repository ppa:solaar-unifying/stable
-
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
-sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-
-sudo apt install 1password -y
+sudo apt install curl
 sudo apt install fonts-firacode -y
-sudo apt install gh -y
-sudo apt install gir1.2-gda-5.0 gir1.2-gsound-1.0 # for pano
 sudo apt install gnome-tweaks -y
-sudo apt install htop -y
-sudo apt install nodejs
+sudo apt install nodejs -y
 sudo apt install papirus-icon-theme -y
-sudo apt install solaar -y
-sudo apt install tilix -y
-sudo apt install transmission -y
-sudp apt install gnome-browser-connector -y
+sudo apt install gnome-browser-connector -y
 
-sudo add-apt-repository multiverse
-sudo apt install steam -y
-
-sudo snap install code --classic
-sudo snap install discord
-sudo snap install docker
+sudo snap install transmission
+sudo snap install steam
 sudo snap install spotify
+sudo snap install nordvpn
+sudo snap install htop
+sudo snap install docker
+sudo snap install discord
+sudo snap install code --classic
+snap install nvtop
+snap connect nvtop:system-observe
+snap connect nvtop:process-control
+snap connect nvtop:kubernetes-support
+snap connect nvtop:hardware-observe
 
-sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
-# setup pnpm
+# download and install Node.js (you may need to restart the terminal)
+nvm install 22
+
+# enable pnpm
 corepack enable pnpm
 
-# setup tilix
-dconf load /com/gexperts/Tilix/ < ./config/tilix.dconf
+# setup gnome terminal profile
+dconf load /org/gnome/terminal/ < gnome_terminal_settings.txt
 
 # setup bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-
 cp -rf ./config/.bashrc ~/.bashrc
 source ~/.bashrc
 
@@ -57,4 +55,10 @@ source ~/.bashrc
 ./setup_code.sh
 
 # setup mx master 3
+sudo add-apt-repository ppa:solaar-unifying/stable
+sudo apt install solaar -y
 cp -rf ./config/solaar_rules.yaml ~/.config/solaar/rules.yaml
+sudo wget https://raw.githubusercontent.com/pwr-Solaar/Solaar/master/rules.d-uinput/42-logitech-unify-permissions.rules -O /etc/udev/rules.d/42-logitech-unify-permissions.rules
+gnome-extensions install https://extensions.gnome.org/extension-data/solaar-extension%40sidevesh.shell-extension.zip
+gnome-extensions enable solaar-extension@sidevesh
+
